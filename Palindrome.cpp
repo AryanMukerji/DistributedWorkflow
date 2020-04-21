338af938ab392ca50f1ccecbf3124ef3f6e6eb78
Sarthak1306 <sarthak13062001@gmail.com>
2020-04-21 16:36:32 +0530

    Added Function in C

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -22,3 +22,22 @@ int main()
     }
     return 0;
 }
+
+// Function in C
+/* void isPalindrome(char str[]) 
+{
+    // Start from leftmost and rightmost corners of str
+    int l = 0;
+    int h = strlen(str) - 1;
+
+    // Keep comparing characters while they are same
+    while (h > l)
+    {
+        if (str[l++] != str[h--])
+        {
+            printf("%s is Not Palindrome", str);
+            return;
+        }
+    }
+    printf("%s is palindrome", str);
+} */
